ad0d2a2b722c1168ec71721022a569cf602cc602
ahmad-smadi <ahmad.smadi4@gmail.com>
2021-06-06 20:05:07 +0300

    html structr, js code, css style cd desktop pwd

---
+++ javascript/app.js
@@ -0,0 +1,22 @@
+let userName = prompt('whats your name');
+    console.log(userName);
+
+    alert (`your name is ${userName}, welcome to our coding world`);
+
+let userAge = prompt('how old are you');
+    console.log(userAge);
+
+alert (`great you have ${userAge}, of coding`);
+
+
+let userExperience = prompt('how many years of programing experience do you have');
+      console.log(userExperience);
+alert ('great')
+
+
+let userIntrest = prompt('whats your fav code language');
+     console.log(userIntrest);
+
+alert (`awsome ${userIntrest}, lets coding `);
+
+
